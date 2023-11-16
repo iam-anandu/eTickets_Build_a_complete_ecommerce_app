faff293a5578101fd89ab9e62da3206daeec82c4
anandukp <anandukp@ANANDUKP-LAP>
2023-11-16 13:33:06 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTickets_Build_a_complete_ecommerce_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
